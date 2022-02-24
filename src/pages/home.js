59da8bcf4f9c65a553ce5ec6c21e5ff1e82c926f
Eliezer <eliezer1789j@gmail.com>
2022-02-24 09:31:20 +0000

    Linking - react-router-dom

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div>
       <h1>Notedly</h1>
       <p>This is the home page</p>
+      {/* Add a list of links */}
+      <ul>
+        <li>
+          <Link to="/mynotes">My Notes</Link>
+        </li>
+        <li>
+          <Link to="/favorites">Favorites</Link>
+        </li>
+      </ul>
     </div>
   );
 };
