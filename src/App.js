6ae1a8b881a35c4d79e13512f540bd58664ea9b0
Eliezer <eliezer1789j@gmail.com>
2022-02-25 16:11:06 +0000

    connect web client with server API - ApolloClient

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import GlobalStyle from "./components/GlobalStyle";
 
 import Pages from "./pages";
 
+// configure our API URI & cache
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+// configure Apollo Client
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true,
+});
+
 function App() {
   return (
-    <div>
+    <ApolloProvider client={client}>
       <GlobalStyle />
       <Pages />
-    </div>
+    </ApolloProvider>
   );
 }
 
