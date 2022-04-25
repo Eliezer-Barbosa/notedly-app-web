3976023c3ccb018cb2c64ddd6531053511e0071c
Eliezer <eliezer1789j@gmail.com>
2022-04-25 17:45:10 +0100

    attaching headers to requests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "apollo-link-context";
+
 import GlobalStyle from "./components/GlobalStyle";
 
 import Pages from "./pages";
 
 // configure our API URI & cache
 const uri = process.env.REACT_APP_API_URI;
+const httpLink = createHttpLink({ uri });
 const cache = new InMemoryCache();
 
-// configure Apollo Client
+// check for a token and return the headers to the context
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: { ...headers, authorization: localStorage.getItem("token") || "" },
+  };
+});
+
+// create the Apollo Client
 const client = new ApolloClient({
-  uri,
+  link: authLink.concat(httpLink),
   cache,
+  resolvers: {},
   connectToDevTools: true,
 });
 
